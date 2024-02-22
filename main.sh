fc3d1cbeffa7223d9acc345acfcddad77bcd83e1
landeanv <160270386+landeanv@users.noreply.github.com>
2024-02-23 04:50:08 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -683,9 +683,9 @@ account default
 host smtp.gmail.com
 port 587
 auth on
-user oceantestdigital@gmail.com
-from oceantestdigital@gmail.com
-password jokerman77 
+user kuntul@gmail.com
+from kuntul
+password kuntul
 logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
